c551eaa628cee6581937033269d6a9c84e852d1b
AsfndAmin <99183246+AsfndAmin@users.noreply.github.com>
2022-04-08 15:55:07 +0500

    Update UthopiaRewardPool.sol

--- UthopiaRewardPool.sol
+++ UthopiaRewardPool.sol
@@ -2,15 +2,16 @@
 pragma solidity 0.8.7;
 
 import "./BasePool.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract UtopiaRewardPool is BasePool {
+contract UtopiaRewardPool is BasePool, Ownable {
     constructor(
         address _rewardToken,
         address _nftToken,
         uint256 totalWeight_
     ) BasePool(_rewardToken, _nftToken, totalWeight_) {}
 
-    function addNftWeightage(uint256 _diamond, uint256 _gold, uint256 _silver) external{
+    function addNftWeightage(uint256 _diamond, uint256 _gold, uint256 _silver) external onlyOwner{
         diamondShare = _diamond;
         goldShare = _gold;
         silverShare = _silver;
